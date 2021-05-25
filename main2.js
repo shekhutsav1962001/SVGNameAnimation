736264d76674b0c85ae9f317353cb9f85d6e6b03
shekhutsav1962001 <shekhutsav1962001@gmail.com>
2021-05-25 20:47:36 +0530

    blog it added

---
+++ main2.js
@@ -0,0 +1,7 @@
+console.log("hello");
+let na = document.querySelectorAll('#name path')
+console.log(na);
+for (let i= 0; i < na.length; i++) {
+    console.log(`Letter ${i} is ${na[i].getTotalLength()}`);
+    
+}
